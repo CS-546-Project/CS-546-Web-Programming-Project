8e81f5ddcd8dac5b5c38a0915cfdda208dfaa063
Harsh Jagdishbhai Kevadia <kevadiyaharsh@gmail.com>
2017-08-14 23:32:45 -0400

    Add Mongo DB connection

--- data/index.js
+++ data/index.js
@@ -3,3 +3,15 @@
  *  Created On : Fri Jul 21 2017
  *  File : index.js
  *******************************************/
+const vendorRoutes = require("./vendors");
+const userRoutes = require("./users");
+
+let constructorMethod = (app) => {
+    app.use("/vendor", vendorRoutes);
+    app.use("/user", userRoutes);
+};
+
+module.exports = {
+    users: require("./users"),
+    posts: require("./vendors")
+};
---
+++ data/users.js
@@ -0,0 +1,5 @@
+/******************************************
+ *  Author : Harsh Jagdishbhai Kevadia   
+ *  Created On : Mon Aug 14 2017
+ *  File : users.js
+ *******************************************/
