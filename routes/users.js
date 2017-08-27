4e86f6d0cb39b4917249595a85ebb91a1c5c25e7
aakriti7 <asrivas5@stevens.edu>
2017-08-27 00:51:22 -0400

    Add files via upload
    adding routes

--- routes/users.js
+++ routes/users.js
@@ -3,3 +3,12 @@
  *  Created On : Tue Aug 15 2017
  *  File : users.js
  *******************************************/
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
--- routes/vendors.js
+++ routes/vendors.js
@@ -3,3 +3,12 @@
  *  Created On : Tue Aug 15 2017
  *  File : vendors.js
  *******************************************/
+var express = require('express');
+var router = express.Router();
+
+/* GET vendors listing. */
+router.get('/', function(req, res) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
