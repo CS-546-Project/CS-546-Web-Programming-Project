cc39f2cbd46db866d312fe59f28e5d42c49b399f
aakriti7 <asrivas5@stevens.edu>
2017-08-27 02:13:40 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,108 +1,20 @@
-/******************************************
- *  Author : Harsh Jagdishbhai Kevadia   
- *  Created On : Fri Jul 21 2017
- *  File : index.js
- *******************************************/
-const vendorRoutes = require("./vendors");
-const userRoutes = require("./users");
-const path = require('path');
+const usersRoutes = require("./users");
+const vendorsRoutes = require("./vendors");
+const hairCuttersRoutes = require("./hairCutters");
+const servicesRoutes = require("./services");
+const tmpRoutes = require("./tmp");
 
 const constructorMethod = (app) => {
-    app.use("/vendors", vendorRoutes);
-    app.use("/", userRoutes);
+    app.use("/users", usersRoutes);
+    app.use("/vendors", vendorsRoutes);
+    app.use("/hairCutters", hairCuttersRoutes);
+    app.use("/services", servicesRoutes);
+        app.use("/tmp", tmpRoutes);
+
 
     app.use("*", (req, res) => {
-        res.redirect("/");
+        res.status(404).json({error: "Not found"});
     });
 };
 
 module.exports = constructorMethod;
-
-//////////////////////////////
-var express = require('express');
-var router = express.Router();
-var db = require('../db');
-var passport = require('passport');
-var theUser = require('../schemas/user');
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET registration page */
-
-router.get('/register', function(req, res){
-	res.render('register')
-});
-
-router.post('/register', function(req, res){
-	// check the database for the username
-	theUser.find({username: req.param('username')}, function(err, data){
-		if(err){
-			console.log(err)
-		}
-		//if there's no data returned, create the user
-		if(!data.length){
-			var newUser = new theUser({
-				username : req.param('username'),
-				password : req.param('password')
-			});
-
-			newUser.save(function(err){
-				if(err){
-					console.log(err)
-				}
-				res.json({message: req.param('username') + ' has been successfully registered'})
-			});
-		// otherwise send an error message
-		} else{
-			res.json({message : 'That username is already taken!'})
-		}
-	});
-});
-
-/* Get Login Page */
-
-router.get('/login', function(req,res){
-	// if a user is already logged in
-    if (req.session.passport.user != undefined) {
-        res.redirect('/user');
-    }
-	res.render('login')
-});
-
-router.post('/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.json({
-                'error': info.message
-            });
-        }
-
-        req.logIn(user, function(err) {
-            if (err) {
-                return next(err);
-            }
-            return res.redirect('/user');
-        });
-    })(req, res, next);
-});
-
-router.get('/user', function(req, res){
-	if (req.session.passport.user == undefined) {
-        res.redirect('/login');
-    } else {
-    	res.render('user', {user: req.user.username});
-    }
-});
-
-router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/login');
-});
-
-module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,62 @@
-/******************************************
- *  Author : Harsh Jagdishbhai Kevadia   
- *  Created On : Tue Aug 15 2017
- *  File : users.js
- *******************************************/
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const data = require("../data");
+const usersData = data.users;
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get("/:id", (req, res) => {
+    usersData.getUserById(req.params.id).then((user) => {
+        res.json(user);
+    }).catch(() => {
+        res.status(404).json({ error: "User not found" });
+    });
 });
 
+
+
+
+router.post("/", (req, res) => {
+    let userBody = req.body;
+
+    usersData.addUser(userBody.firstName, userBody.lastName, userBody.address, userBody.contactNumber, 
+    userBody.state, userBody.city, userBody.zipCode, userBody.email, userBody.password)
+        .then((newUser) => {
+            res.json(newUser);
+        }).catch((e) => {
+            res.status(500).json({ error: e });
+        });
+});
+
+router.put("/:id", (req, res) => {
+    let updatedData = req.body;
+
+    let getUser = usersData.getUserById(req.params.id);
+
+    getUser.then(() => {
+        return usersData.updateUserInfo(req.params.id, updatedData)
+            .then((updatedUser) => {
+                res.json(updatedUser);
+            }).catch((e) => {
+                res.status(500).json({ error: e });
+            });
+    }).catch((e) => {
+console.log(e);
+        res.status(404).json({ error: "User not found" });
+    });
+
+});
+
+router.delete("/:id", (req, res) => {
+    let getUser = usersData.getUserById(req.params.id);
+
+    getUser.then(() => {
+        return usersData.removeUser(req.params.id)
+            .then(() => {
+                res.sendStatus(200);
+            }).catch((e) => {
+                res.status(500).json({ error: e });
+            });
+    }).catch(() => {
+        res.status(404).json({ error: "User not found" });
+    });
+});
 module.exports = router;
